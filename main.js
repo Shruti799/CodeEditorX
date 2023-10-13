f9171336b3cf2090e5a09f039de60546dcd97664
Shruti799 <shrutijwal999@gmail.com>
2023-10-13 21:12:44 +0530

    updated

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function toggleSignUpForm() {
+    const signUpForm = document.getElementById('signup-form');
+    if (signUpForm.style.display === 'none' || signUpForm.style.display === '') {
+        signUpForm.style.display = 'block';
+    } else {
+        signUpForm.style.display = 'none';
+    }
+}
+
 function run(){
     let htmlCode = document.getElementById("html-code").value;
     let cssCode = document.getElementById("css-code").value;
@@ -7,3 +16,16 @@ function run(){
     output.contentDocument.body.innerHTML = htmlCode +"<style>"+cssCode+"</style>";
     output.contentWindow.eval(jsCode);
 }
+
+function clearPlaceholder(textarea) {
+    if (textarea.value === textarea.defaultValue) {
+      textarea.value = '';
+    }
+  }
+  
+function restorePlaceholder(textarea) {
+  if (textarea.value === '') {
+    textarea.value = textarea.defaultValue;
+  }
+}
+
